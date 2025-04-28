8c72d9647209f203e7c3648d4590c1089d786d0f
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-04-28 10:08:40 -0500

    add sambanova sample run local tools

---
+++ examples/sample_run_local_tools_sambanova.py
@@ -0,0 +1,67 @@
+"""Sambanova Sample"""
+import os
+from typing import List
+from dotenv import load_dotenv
+from openai import OpenAI
+from toolhouse import Toolhouse
+
+#  Make sure to set up the .env file according to the .env.example file.
+load_dotenv()
+
+TH_API_KEY = os.getenv("TOOLHOUSE_API_KEY")
+SAMBANOVA_API_KEY = os.getenv("SAMBANOVA_API_KEY")
+
+
+local_tools = [
+    {'type': 'function',
+     'function':
+         {
+             'name': 'hello',
+             'description': 'The user receives a customized hello message from a city and returns it to the user.',
+             'parameters': {
+                 'type': 'object',
+                 'properties': {
+                     'city': {'type': 'string', 'description': 'The city where you are from'}
+                 }},
+             'required': ['city']
+         }}]
+
+# Initialize Toolhouse with the OpenAI provider
+th = Toolhouse(api_key=TH_API_KEY, provider="openai")
+th.set_metadata("id", "fabio")
+th.set_metadata("timezone", 5)
+
+
+@th.register_local_tool("hello")
+def hello_tool(city: str):
+    """Return a Hello message from a specific city."""
+    return f"Hello from {city}!!!"
+
+
+client = OpenAI(
+    api_key=SAMBANOVA_API_KEY,
+    base_url="https://api.sambanova.ai/v1/",
+)
+
+messages: List = [{
+    "role": "user",
+    "content":
+        "Can I get a hello from Rome?"
+    }]
+
+response = client.chat.completions.create(
+    model='Meta-Llama-3.1-405B-Instruct',
+    messages=messages,
+    tools=th.get_tools() + local_tools # Retrieve tools installed from Toolhouse
+)
+
+# Run the tools using the Toolhouse client with the created message
+messages += th.run_tools(response)
+
+response = client.chat.completions.create(
+            model='Meta-Llama-3.1-405B-Instruct',
+            messages=messages,
+            tools=th.get_tools() + local_tools # Retrieve tools installed from Toolhouse
+        )
+
+print(response.choices[0].message.content)
